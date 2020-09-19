55e4ec2164a4ce09794786146f6034b8f2c6fac5
leolecrado <leocrapart@yahoo.fr>
2020-09-19 12:51:15 +0200

    3

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,8 +1,8 @@
 <template>
 	<div class="bg-gray-200 bg-opacity-75 flex flex-col rounded">
-		<div class="relative" style="padding-bottom: 50%">
+		<div class="relative rounded-t-lg" style="padding-bottom: 50%">
 			<img :src="imagePath" alt="No image corresponding"
-			class="absolute w-full h-full object-cover">
+			class="absolute w-full h-full object-cover rounded-t">
 		</div>
 		<div class="py-1 px-4 font-semibold text-center">{{ title }}</div>
 	</div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,10 +2,7 @@
   <div>
 	<PresentationCard class="m-4" />
 	<div class="grid grid-cols-2 gap-4 m-4">
-		<ProductCard imagePath="/img/confiture-figues.jpg" title="noix de coco"/>
-		<ProductCard imagePath="/img/confiture-figues.jpg" title="noix de coco"/>
-		<ProductCard imagePath="/img/confiture-figues.jpg" title="noix de coco"/>
-		<ProductCard imagePath="/img/confiture-figues.jpg" title="noix de coco"/>
+		<ProductCard v-for="i of array" :key="i" imagePath="/img/confiture-figues.jpg" title="Figues"/>
 	</div>
 
   </div>
@@ -19,6 +16,10 @@ export default {
   components: {
 	PresentationCard,
 	ProductCard
+  },
+  setup() {
+	const array = [1,2,3,4,5,6,7,8,9,10]
+	return { array }
   }
 }
 </script>
